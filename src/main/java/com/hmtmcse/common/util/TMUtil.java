7dfd043244510f485ae678f823cd650700116571
Touhid Mia <hmtm.cse@gmail.com>
2018-12-23 21:04:31 +0600

    Reader

--- src/main/java/com/hmtmcse/common/util/TMUtil.java
+++ src/main/java/com/hmtmcse/common/util/TMUtil.java
@@ -2,7 +2,10 @@ package com.hmtmcse.common.util;
 
 
 import com.hmtmcse.common.TMConfigHolder;
+import sun.misc.ExtensionInstallationException;
+
 import java.io.File;
+import java.util.List;
 import java.util.Map;
 
 public class TMUtil {
@@ -35,6 +38,45 @@ public class TMUtil {
             return mapKeyValue;
         }
         return null;
+    }
+
+    public static void printMap(Map<Object, Object> inputMap){
+        printMap(inputMap, "-");
+    }
+
+    public static void printMap(Map<Object, Object> inputMap, String depth) {
+        for (Map.Entry<Object, Object> entry : inputMap.entrySet()) {
+            Object key = entry.getKey();
+            Object value = entry.getValue();
+            if (value instanceof Map) {
+                Map<Object, Object> subMap = (Map<Object, Object>) value;
+                printMap(subMap, depth + "-");
+            } else if (value instanceof List) {
+                printMapList((List<Map>) value, depth + "-");
+            } else if (value instanceof Object) {
+                TMUtil.print(depth + " " + key + " : " + value);
+            } else {
+                TMUtil.print("Map Error: " + key + " : " + value);
+            }
+        }
+    }
+
+    public static void printMapList(List<Map> inputList, String depth) {
+        for (Map map : inputList) {
+            try{
+                if (map instanceof List){
+                    printMapList((List<Map>) map, depth + "-");
+                }else if (map instanceof Map){
+                    printMap((Map) map, depth + "-");
+                } else {
+                    Object key = map.keySet();
+                    Object value = map.get(key);
+                    TMUtil.print("List Error: " + key + " : " + value);
+                }
+            }catch (Exception e){
+                TMUtil.print("List Error: " + e.getMessage());
+            }
+        }
     }
 
 
