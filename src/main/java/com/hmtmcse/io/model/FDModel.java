c7af4295de9eaf7707c9d289887f2b2d973d73ab
Touhid Mia <hmtm.cse@gmail.com>
2018-12-26 21:44:39 +0600

    Shell Util

--- src/main/java/com/hmtmcse/io/model/FDModel.java
+++
@@ -1,9 +0,0 @@
-package com.hmtmcse.io.model;
-
-import com.hmtmcse.io.exception.TmIoException;
-
-public interface FDModel {
-
-    Boolean copy(String source, String destination) throws TmIoException;
-
-}
