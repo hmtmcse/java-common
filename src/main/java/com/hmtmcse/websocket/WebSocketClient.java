66ff8605af7234177c178c3b214248a265d0d690
Touhid Mia <hmtm.cse@gmail.com>
2019-01-02 10:24:10 +0600

    Shell Util

--- src/main/java/com/hmtmcse/websocket/WebSocketClient.java
+++ src/main/java/com/hmtmcse/websocket/WebSocketClient.java
@@ -15,11 +15,32 @@ import javax.websocket.Session;
 @ClientEndpoint
 public class WebSocketClient {
 
+    private Session session;
+
     public WebSocketClient(String uri) throws TMException {
         try {
             WebSocketContainer container = ContainerProvider.getWebSocketContainer();
             container.connectToServer(this, new URI(uri));
         } catch (Exception ex) {
+            throw new TMException(ex.getMessage());
+        }
+    }
+
+
+    @OnOpen
+    public void onOpen(Session session) {
+        this.session = session;
+    }
+
+
+    @OnMessage
+    public void onMessage(String message, Session session) {}
+
+
+    public void sendMessage(String message) throws TMException {
+        try {
+            session.getBasicRemote().sendText(message);
+        } catch (IOException ex) {
             throw new TMException(ex.getMessage());
         }
     }
