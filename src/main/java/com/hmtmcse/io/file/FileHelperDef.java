3597bb6276d3e3a162dd53788fd63207012bb5d2
Touhid Mia <hmtm.cse@gmial.com>
2019-03-21 12:33:15 +0600

    File Helper Definitions

---
+++ src/main/java/com/hmtmcse/io/file/FileHelperDef.java
@@ -0,0 +1,41 @@
+package com.hmtmcse.io.file;
+
+public interface FileHelperDef {
+
+    public FileHelperResponseDef copy(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef removeThenCopy(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef copyAllNotLink(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef copyAll(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef copyOverwriteAll(FileHelperRequestDef requestDef) throws FileHelperException;
+
+    public FileHelperResponseDef move(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef removeThenMove(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef moveAllNotLink(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef moveAll(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef moveOverwriteAll(FileHelperRequestDef requestDef) throws FileHelperException;
+
+
+    public FileHelperResponseDef delete(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef deleteIfExist(FileHelperRequestDef requestDef) throws FileHelperException;
+
+
+    public FileHelperResponseDef changeAttributes(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef getAttributes(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef changeAllAttributes(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef makeDir(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef removeAndMakeDir(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef rename(FileHelperRequestDef requestDef) throws FileHelperException;
+
+
+    public FileHelperResponseDef createSoftLink(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef removeSoftLink(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef createHardLink(FileHelperRequestDef requestDef) throws FileHelperException;
+    public FileHelperResponseDef removeHardLink(FileHelperRequestDef requestDef) throws FileHelperException;
+
+    public Boolean isExist(String path) throws FileHelperException;
+    public Boolean isExistDirectory(String path) throws FileHelperException;
+
+
+    public FileHelperResponseDef listAllFileAndDirectory(FileHelperRequestDef requestDef) throws FileHelperException;
+
+}
---
+++ src/main/java/com/hmtmcse/io/file/FileHelperException.java
@@ -0,0 +1,15 @@
+package com.hmtmcse.io.file;
+
+/**
+ * Created by Touhid Mia on 11/09/2014.
+ */
+public class FileHelperException extends Exception {
+
+    public FileHelperException(){
+        super("File Helper Exception");
+    }
+
+    public FileHelperException(String message){
+        super(message);
+    }
+}
